233b16134c347c328a292024e9f151031d1c43f9
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-05-03 00:33:32 +0600

    After approving author post Notifying to Author

---
+++ app/Notifications/AuthorPostApprove.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class AuthorPostApprove extends Notification
+{
+    use Queueable;
+
+    public $post;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($post)
+    {
+        $this->post=$post;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting('Hello'.$this->post->user->name.'!')
+                    ->subject('Your New Post has Approved by Admin')
+                    ->line('The Post Title was:'.$this->post->title)
+                    ->line('To visit your post click the view Button')
+                    ->action('View Post', url(route('author.post.show',$this->post->id)) )
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
