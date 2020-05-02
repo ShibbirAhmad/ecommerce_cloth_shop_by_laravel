37ab8800918e490ddfac93597ca28dc678fc8e80
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-05-02 10:46:50 +0600

    Author Posts Display board implemented

---
+++ app/Http/Controllers/Author/PostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Author;
+use Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Post;
+use App\Tag;
+use App\Category;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       
+        $posts=Auth::user()->posts()->latest()->get();
+        return view ('author.post.index',compact(['posts']));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
---
+++ resources/views/author/post/index.blade.php
@@ -0,0 +1,87 @@
+@extends('backend/master')
+
+
+@push('css')
+    
+@endpush
+
+
+
+
+@section('content')
+ 
+
+
+
+<a href="{{route('author.post.create')}}" class="btn btn-primary m-2" >Add New Post</a>
+    <div class="card">
+            <div class="card-body">
+                       
+ <table class="table table-bordered text-center ">
+        <thead>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>View <i class="fa fa-eye"> </i> </th>
+                <th>Status</th>
+                <th>Is_Approved</th>
+                <th colspan="2" >Action</th>
+        </thead>  
+        <tbody>
+                @foreach ($posts as $data)
+                <tr>
+                       <td>{{$data->id}}</td> 
+                       <td>{{$data->title}}</td>
+                       <td>{{$data->user->name }}</td>
+                       <td> {{$data->view_count }} </td>
+                       <td>
+                               @if ($data->status==true)
+                                   <span class="badge bg-blue">Published</span>
+                               @else
+                               <span class="badge bg-pink">Pending</span> 
+                               @endif
+                       </td>
+                       <td>
+                        @if ($data->is_approved==true)
+                        <span class="badge bg-blue">Approved</span>
+                    @else
+                    <span class="badge bg-pink">Pending</span> 
+                    @endif   
+                       </td>
+                       <td colspan="2">
+                            <a href="{{route('author.post.show',$data->id)}}"  class="btn btn-dark "><i class="fas fa-eye"></i></a>   
+                            <a href="{{route('author.post.edit',$data->id)}}" style="margin-right:70px;" class="btn btn-success "><i class="fas fa-edit"></i></a>
+                      
+                       <div style="margin-top:-33px; margin-left:55px" >
+                           {!! Form::open(['method'=>'DELETE', 'route'=> ['author.post.destroy',$data->id ]] ) !!}
+                            <button class="btnDelete btn btn-danger"><i class="fa fa-trash-alt"></i></button>
+                           {!! Form::close() !!}
+                       </div> 
+                    
+                    </td>            
+                </tr>
+
+                    
+                @endforeach
+        </tbody>
+</table>
+
+            </div>
+    </div>
+ 
+
+
+
+
+@endsection
+
+
+
+
+@section('script')
+  <script> 
+ 
+
+</script>
+ 
+@endsection()
